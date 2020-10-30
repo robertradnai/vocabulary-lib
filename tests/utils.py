42dfe11c88ed7bdc3287a7e3c9b0b436d6ca781b
Robert Radnai <robert.radnai@gmail.com>
2020-10-30 23:11:58 +0100

    Added debug info

--- tests/utils.py
+++ tests/utils.py
@@ -9,8 +9,10 @@ TEST_DICT_OUT_PATH = "testdata_temp/testdict_out.xlsx"
 def reset_test_env():
     # Delete and re-create test data folder so that the app can't accidentally
     #  overwrite the original test data.
-    test_data = 'testdata'
-    temp_folder = 'testdata_temp'
+    test_data = os.path.abspath('testdata')
+    temp_folder = os.path.abspath('testdata_temp')
+    print(f"Folder for test data: {test_data}")
+    print(f"Folder for temporary test files: {temp_folder}")
     if os.path.exists(temp_folder):
         shutil.rmtree(temp_folder)
     shutil.copytree(test_data, temp_folder)
